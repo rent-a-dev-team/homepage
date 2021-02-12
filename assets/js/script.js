d0cfa0fe0a7ab4a76c7205e154d56859da52f421
Nils Bokermann <nils.bokermann@bermuda.de>
2021-02-12 15:42:51 +0100

    Etwas Javascript rausgeschmissen, was nicht benötigt wurde

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,6 @@ jQuery(function ($) {
     });
 
     /* ========================================================================= */
-    /*	Magnific popup
-    /* =========================================================================  */
-    $('.image-popup').magnificPopup({
-        type: 'image',
-        removalDelay: 160, //delay removal by X to allow out-animation
-        callbacks: {
-            beforeOpen: function () {
-                // just a hack that adds mfp-anim class to markup
-                this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-                this.st.mainClass = this.st.el.attr('data-effect');
-            }
-        },
-        closeOnContentClick: true,
-        midClick: true,
-        fixedContentPos: false,
-        fixedBgPos: true
-    });
-    /* ========================================================================= */
     /*	Portfolio Filtering Hook
     /* =========================================================================  */
 
